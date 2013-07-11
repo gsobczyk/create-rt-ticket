f0f6663ad40902237e9d0006f6edbe44943fee81
Grzegorz Sobczyk <grzegorz.sobczyk@unity.pl>
2013-07-11 12:32:49 +0200

    pierwszy commit

---
+++ src/common/main.js
@@ -0,0 +1,18 @@
+﻿//function MyExtension() {
+//	var self = this;
+//	kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function() {
+//		self._onCommand();
+//	});
+//}
+//
+//MyExtension.prototype = {
+//
+//	_onCommand: function() {
+//		kango.browser.tabs.create({url: 'http://kangoextensions.com/'});
+//	}
+//};
+//
+//var extension = new MyExtension();
+
+//kango.ui.browserButton.setPopup({url:'popup.html', width: 600, height:520})
+//kango.ui.browserButton.setPopup(null)
